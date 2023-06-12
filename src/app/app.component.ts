899be14728721fc8b5eb9b6b4d4a4cfb55d89512
Puvistyne Rajasegar <puvistien@gmail.com>
2023-06-12 13:40:36 +0200

    src

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+/**
+ * Import the members of the Angular core library that are needed in this component.
+ */
+import { Component } from '@angular/core';
+
+/**
+ * @Component is a decorator function that specifies the Angular metadata for the component.
+ * The CLI generated three metadata properties:
+ * 1. selector— the component's CSS element selector
+ * 2. templateUrl— the location of the component's template file.
+ * 3. styleUrls— the location of the component's private CSS styles.
+ *
+ * This is the decorator of this component app.component.ts.
+ */
+@Component({
+  selector: 'pm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+/**
+ * This is the class of this component.
+ */
+export class AppComponent {
+  title = 'Angular: Getting Started';
+}
